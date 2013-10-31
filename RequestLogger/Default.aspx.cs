edec24c0883d33738f9f08232137af58a5e34ee2
Bjørn Otto Vasbotten <bjorn@pswin.com>
2013-10-31 16:08:42 +0100

    Initial commit

---
+++ RequestLogger/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ExtensionMethods;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using log4net;
+
+namespace RequestLogger
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private static ILog log = LogManager.GetLogger("Default");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var request = HttpContext.Current.Request;
+            log.Info("Receiving request...");
+            log.Info(request.ToRaw());
+
+            Response.StatusCode = 200;
+            Response.StatusDescription = "OK";
+        }
+    }
+}
